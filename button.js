2eedda1f5fe78d2776fbb799c42581e4cac4e3c8
krutik-24 <avitikrutik1@gmail.com>
2025-04-14 19:57:13 +0530

    Create button.js

---
+++ button.js
@@ -0,0 +1,6 @@
+<script>
+    // Example JavaScript for interactivity
+    document.querySelector('button').addEventListener('click', function() {
+        alert('Button clicked!');
+    });
+</script>
